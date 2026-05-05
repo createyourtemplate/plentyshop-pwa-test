2b1d8e86ab29f81ac7bd2591dfc1aca9939f2b01
Ihab Hussein <ihabh@ihabh.de>
2026-05-05 16:23:22 +0200

    feat: update Google Tag Manager integration to use environment variable for tracking ID

--- apps/web/modules/tracking/index.ts
+++ apps/web/modules/tracking/index.ts
@@ -6,12 +6,12 @@ export default defineNuxtModule({
   },
   setup(_options, nuxt) {
     const resolver = createResolver(import.meta.url);
-    addPlugin(resolver.resolve('./runtime/gtm.plugin'));
+    addPlugin({ src: resolver.resolve('./runtime/gtm.plugin'), mode: 'server' });
 
     nuxt.options.app.head.script = nuxt.options.app.head.script ?? [];
     nuxt.options.app.head.script.push({
-      innerHTML: `<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KG7DWQ4M"
-height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>`,
+      innerHTML: `<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=${process.env.NUXT_PUBLIC_GOOGLE_ANALITICS_TRACKING_ID}"
+      height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>`,
       tagPosition: 'bodyOpen',
     });
   },
--- apps/web/modules/tracking/runtime/gtm.plugin.ts
+++ apps/web/modules/tracking/runtime/gtm.plugin.ts
@@ -2,15 +2,16 @@ export default defineNuxtPlugin(() => {
   useHead({
     script: [
       {
+        type: 'text/javascript',
         innerHTML: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer', ${process.env.NUXT_PUBLIC_GOOGLE_ANALITICS_TRACKING_ID});`,
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer', '${process.env.NUXT_PUBLIC_GOOGLE_ANALITICS_TRACKING_ID}');`,
         tagPosition: 'bodyOpen',
         tagPriority: 'low',
-        id: 'gtm'
+        id: 'gtm',
       },
-    ],
+    ]
   });
 });
