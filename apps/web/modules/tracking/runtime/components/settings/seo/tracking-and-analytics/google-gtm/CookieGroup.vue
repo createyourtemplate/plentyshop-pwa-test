19c49a794868774df08ae15fdf696903ea5177ce
seoambulance <125657912+seoambulance@users.noreply.github.com>
2026-08-19 13:06:59 +0200

    Update CookieGroup.vue

--- apps/web/modules/tracking/runtime/components/settings/seo/tracking-and-analytics/google-gtm/CookieGroup.vue
+++ apps/web/modules/tracking/runtime/components/settings/seo/tracking-and-analytics/google-gtm/CookieGroup.vue
@@ -49,10 +49,10 @@ const googleGtmCookieGroup = computed({
     "placeholder": "Select Cookie Group"
   },
   "de": {
-    "label": "Cookie Group",
-    "description1": "⚠️ This group of settings will require a shop redeploy to take effect.",
-    "description2": "Control if and how you want to use Google GTM.",
-    "placeholder": "Select Cookie Group"
+    "label": "Cookie Gruppe",
+    "description1": "⚠️ Damit diese Einstellungen der Gruppe wirksam wird, ist eine erneute Bereitstellung des Shops erforderlich.",
+    "description2": "Steuern Sie, ob und wie Sie Google GTM nutzen möchten.",
+    "placeholder": "Cookie Gruppe auswählen"
   }
 }
 </i18n>
