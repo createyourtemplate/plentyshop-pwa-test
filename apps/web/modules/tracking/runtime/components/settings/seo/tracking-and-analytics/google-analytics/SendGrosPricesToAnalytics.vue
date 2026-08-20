7514183f19f3bc7006e52bb0d2cd1b6fc24c3d7d
seoambulance <125657912+seoambulance@users.noreply.github.com>
2026-08-20 14:15:49 +0200

    Update SendGrosPricesToAnalytics.vue

--- apps/web/modules/tracking/runtime/components/settings/seo/tracking-and-analytics/google-analytics/SendGrosPricesToAnalytics.vue
+++ apps/web/modules/tracking/runtime/components/settings/seo/tracking-and-analytics/google-analytics/SendGrosPricesToAnalytics.vue
@@ -5,7 +5,7 @@
         {{ getEditorTranslation('label') }}
       </UiFormLabel>
       <SfSwitch
-        v-model="sendGrossPricesToCytGoogleAds"
+        v-model="sendGrossPricesToGoogleAnalytics"
         class="checked:bg-editor-button checked:before:hover:bg-editor-button checked:border-gray-500 checked:hover:border:bg-gray-700 hover:border-gray-700 hover:before:bg-gray-700 checked:hover:bg-gray-300 checked:hover:border-gray-400"
       />
     </div>
@@ -14,9 +14,9 @@
 <script setup lang="ts">
 import { SfSwitch } from '@storefront-ui/vue';
 
-const { updateSetting, getSetting } = useSiteSettings('sendGrossPricesToCytGoogleAds');
+const { updateSetting, getSetting } = useSiteSettings('sendGrossPricesToGoogleAnalytics');
 
-const sendGrossPricesToCytGoogleAds = computed({
+const sendGrossPricesToGoogleAnalytics = computed({
   get: () => !!getSetting(),
   set: (value) => updateSetting(value.toString()),
 });
