fec7763ff9b1680bb95d6e2320aaca92db3c3801
seoambulance <125657912+seoambulance@users.noreply.github.com>
2026-08-20 13:56:24 +0200

    Update SendGrosPricesToAnalytics.vue

--- apps/web/modules/tracking/runtime/components/settings/seo/tracking-and-analytics/google-analytics/SendGrosPricesToAnalytics.vue
+++ apps/web/modules/tracking/runtime/components/settings/seo/tracking-and-analytics/google-analytics/SendGrosPricesToAnalytics.vue
@@ -1,1 +1,34 @@
+<template>
+  <div class="py-2">
+    <div class="flex justify-between mb-2">
+      <UiFormLabel class="mb-1">
+        {{ getEditorTranslation('label') }}
+      </UiFormLabel>
+      <SfSwitch
+        v-model="sendGrossPricesToCytGoogleAds"
+        class="checked:bg-editor-button checked:before:hover:bg-editor-button checked:border-gray-500 checked:hover:border:bg-gray-700 hover:border-gray-700 hover:before:bg-gray-700 checked:hover:bg-gray-300 checked:hover:border-gray-400"
+      />
+    </div>
+  </div>
+</template>
+<script setup lang="ts">
+import { SfSwitch } from '@storefront-ui/vue';
 
+const { updateSetting, getSetting } = useSiteSettings('sendGrossPricesToCytGoogleAds');
+
+const sendGrossPricesToCytGoogleAds = computed({
+  get: () => !!getSetting(),
+  set: (value) => updateSetting(value.toString()),
+});
+</script>
+
+<i18n lang="json">
+{
+  "en": {
+    "label": "Send Gross Prices to google analytics"
+  },
+  "de": {
+    "label": "Send Gross Prices to google analytics"
+  }
+}
+</i18n>
