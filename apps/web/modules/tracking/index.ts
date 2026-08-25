adbc6abfbd20e6ed8bec039f276fbe484ecabee6
seoambulance <125657912+seoambulance@users.noreply.github.com>
2026-08-25 16:30:30 +0200

    Update index.ts

--- apps/web/modules/tracking/index.ts
+++ apps/web/modules/tracking/index.ts
@@ -31,10 +31,12 @@ export default defineNuxtModule({
     const publicRuntimeConfig = nuxt.options.runtimeConfig.public as Record<string, unknown>;
 
     publicRuntimeConfig.enableCytGA = process.env.NUXT_PUBLIC_ENABLE_CYT_G_A === 'true';
+    publicRuntimeConfig.googleCytGACookieGroup = process.env.NUXT_PUBLIC_GOOGLE_ADS_CYT_G_A_COOKIE_GROUP || 'Cyt.cookieBar.statistics.label';
     publicRuntimeConfig.googleCytGACookiesToRegister = process.env.NUXT_PUBLIC_GOOGLE_CYT_G_A_COOKIES_TO_REGISTER || '';
+    publicRuntimeConfig.registerCytGACookieAsOptOut = process.env.NUXT_PUBLIC_REGISTER_CYT_G_A_COOKIE_AS_OPT_OUT === 'false';
 
     publicRuntimeConfig.enableGoogleAds = process.env.NUXT_PUBLIC_ENABLE_GOOGLE_ADS === 'true';
-    publicRuntimeConfig.googleAdsCookieGroup = process.env.NUXT_PUBLIC_GOOGLE_ADS_COOKIE_GROUP || 'Cyt.cookieBar.statistics.label';
+    publicRuntimeConfig.googleAdsCookieGroup = process.env.NUXT_PUBLIC_GOOGLE_ADS_COOKIE_GROUP || 'Cyt.cookieBar.marketing.label';
     publicRuntimeConfig.googleAdsCookiesToRegister = process.env.NUXT_PUBLIC_GOOGLE_ADS_COOKIES_TO_REGISTER || '';
     publicRuntimeConfig.registerAdsCookieAsOptOut = process.env.NUXT_PUBLIC_REGISTER_ADS_COOKIE_AS_OPT_OUT === 'false';
     
