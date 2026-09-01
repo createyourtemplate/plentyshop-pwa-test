4956954edb3b9d2c19377a4a05c8d1314a35bcca
seoambulance <125657912+seoambulance@users.noreply.github.com>
2026-09-01 13:47:35 +0200

    Update View.vue

--- apps/web/modules/gtm-tracking/runtime/components/settings/gtm-tracking/google-tag-manager/View.vue
+++ apps/web/modules/gtm-tracking/runtime/components/settings/gtm-tracking/google-tag-manager/View.vue
@@ -1,7 +1,21 @@
 <template>
   <SiteConfigurationView>
-    <template #setting-title>{{ getEditorUITranslation('google-tag-manager') }}</template>
+    <template #setting-breadcrumbs>{{ getEditorTranslation('breadcrumbs') }}</template>
+    <template #setting-title>{{ getEditorTranslation('label') }}</template>
   </SiteConfigurationView>
 </template>
 
 <script setup lang="ts"></script>
+
+<i18n lang="json">
+{
+  "en": {
+    "breadcrumbs": "GTM Tracking",
+    "label": "Google Tag Manager"
+  },
+  "de": {
+    "breadcrumbs": "GTM Tracking",
+    "label": "Google Tag Manager"
+  }
+}
+</i18n>
