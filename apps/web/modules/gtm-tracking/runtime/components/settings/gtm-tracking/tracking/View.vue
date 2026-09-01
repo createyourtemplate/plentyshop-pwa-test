7b4c02dd8fa9f6c30f6aa6040af840a775aa7aec
seoambulance <125657912+seoambulance@users.noreply.github.com>
2026-09-01 15:22:49 +0200

    Update View.vue

--- apps/web/modules/gtm-tracking/runtime/components/settings/gtm-tracking/tracking/View.vue
+++ apps/web/modules/gtm-tracking/runtime/components/settings/gtm-tracking/tracking/View.vue
@@ -9,14 +9,10 @@
 <i18n lang="json">
 {
   "en": {
-    "label": "GTM Tracking",
-    "description1": "⚠️ This group of settings will require a shop redeploy to take effect.",
-    "description2": "Control if and how you want to use Google Tag Manager."
+    "label": "GTM Tracking"
   },
   "de": {
-    "label": "GTM Tracking",
-    "description1": "⚠️ Damit diese Einstellungen der Gruppe wirksam wird, ist eine erneute Bereitstellung des Shops erforderlich.",
-    "description2": "Steuern Sie, ob und wie Sie Google Tag Manager nutzen möchten."
+    "label": "GTM Tracking"
   }
 }
 </i18n>
