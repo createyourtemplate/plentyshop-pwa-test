0c9b551ae44b331ed8d25b467aece1bd2eaaa90a
seoambulance <125657912+seoambulance@users.noreply.github.com>
2026-09-01 11:13:11 +0200

    Create gtm-events.client.ts

---
+++ apps/web/modules/gtm-tracking/runtime/plugins/gtm-events.client.ts
@@ -0,0 +1,48 @@
+import { orderGetters } from '@plentymarkets/shop-api';
+import { findPhoneOptionValue, getCountryName } from '../utils/utils';
+
+export default defineNuxtPlugin(() => {
+  const { on } = usePlentyEvent();
+
+  on('frontend:orderCreated', (order: any) => {
+    if (order?.order && order?.totals) {
+      const billingAddress = orderGetters.getBillingAddress(order);
+      const customerPhone = findPhoneOptionValue(billingAddress?.options);
+      const totalVat = order.totals.vats?.reduce((acc: number, vat: { value: number }) => acc + vat.value, 0) || 0;
+
+      window.dataLayer = window.dataLayer || [];
+      
+      // 1. Ecommerce-Objekt vor dem Event leeren
+      window.dataLayer.push({ ecommerce: null });
+
+      // 2. Purchase Event abschicken
+      window.dataLayer.push({
+        event: 'purchase',
+        ecommerce: {
+          transaction_id: String(orderGetters.getId(order)),
+          value: order.totals.totalGross,
+          currency: order.totals.currency,
+          tax: totalVat,
+          shipping: order.totals.shippingGross,
+          items: order.order.orderItems?.map((item: any) => ({
+            item_id: String(orderGetters.getItemVariationId(item)),
+            item_name: orderGetters.getItemName(item),
+            price: item.amounts?.[0]?.priceGross ?? 0,
+            quantity: orderGetters.getItemQty(item),
+            affiliation: item.referrerId?.toString(),
+          })) || [],
+        },
+        customer_info: {
+          firstname: billingAddress?.name2,
+          lastname: billingAddress?.name3,
+          street: `${billingAddress?.address1 || ''} ${billingAddress?.address2 || ''}`.trim(),
+          phone: customerPhone,
+          email: orderGetters.getOrderEmail(order),
+          zip: billingAddress?.postalCode,
+          city: billingAddress?.town,
+          country: getCountryName(billingAddress?.countryId),
+        },
+      });
+    }
+  });
+});
