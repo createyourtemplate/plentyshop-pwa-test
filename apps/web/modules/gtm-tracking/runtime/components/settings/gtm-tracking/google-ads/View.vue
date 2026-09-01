a3b3c06a6d950ef46b7ccf35cf8620aeacea588e
seoambulance <125657912+seoambulance@users.noreply.github.com>
2026-09-01 13:18:48 +0200

    Create View.vue

---
+++ apps/web/modules/gtm-tracking/runtime/components/settings/gtm-tracking/google-ads/View.vue
@@ -0,0 +1,7 @@
+<template>
+  <SiteConfigurationView>
+    <template #setting-title>{{ getEditorUITranslation('google-ads') }}</template>
+  </SiteConfigurationView>
+</template>
+
+<script setup lang="ts"></script>
