e6047d4b0f566cbc54bcff6cd3a35f7f61c837e7
seoambulance <125657912+seoambulance@users.noreply.github.com>
2026-09-01 13:49:14 +0200

    Update View.vue

--- apps/web/modules/gtm-tracking/runtime/components/settings/gtm-tracking/google-analytics/View.vue
+++ apps/web/modules/gtm-tracking/runtime/components/settings/gtm-tracking/google-analytics/View.vue
@@ -5,3 +5,16 @@
 </template>
 
 <script setup lang="ts"></script>
+
+<i18n lang="json">
+{
+  "en": {
+    "breadcrumbs": "GTM Tracking",
+    "label": "Google Analytics (GA4)"
+  },
+  "de": {
+    "breadcrumbs": "GTM Tracking",
+    "label": "Google Analytics (GA4)"
+  }
+}
+</i18n>
