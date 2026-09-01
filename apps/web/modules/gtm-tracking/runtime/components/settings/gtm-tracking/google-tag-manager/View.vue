c7ba9ad9c1ba8caed5e19e33d39b3e604bacd5e5
seoambulance <125657912+seoambulance@users.noreply.github.com>
2026-09-01 13:16:10 +0200

    Create View.vue

---
+++ apps/web/modules/gtm-tracking/runtime/components/settings/gtm-tracking/google-tag-manager/View.vue
@@ -0,0 +1,6 @@
+<template>
+  <SiteConfigurationView>
+    <template #setting-title>{{ getEditorUITranslation('gtm-tracking') }}</template>
+  </SiteConfigurationView>
+</template>
+<script setup lang="ts"></script>
