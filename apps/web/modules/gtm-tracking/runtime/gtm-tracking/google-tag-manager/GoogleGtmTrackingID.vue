cf7ae98ce122a95d021157b91c5d9b93f45ca617
seoambulance <125657912+seoambulance@users.noreply.github.com>
2026-09-01 11:22:59 +0200

    Create GoogleGtmTrackingID.vue

---
+++ apps/web/modules/gtm-tracking/runtime/gtm-tracking/google-tag-manager/GoogleGtmTrackingID.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="py-2">
+    <div class="flex justify-between mb-2">
+      <UiFormLabel>{{ getEditorTranslation('label') }}</UiFormLabel>
+    </div>
+    <label>
+      <SfInput
+        v-model="googleGtmTrackingId"
+        type="text"
+        data-testid="google-gtm-tracking-id"
+        :placeholder="getEditorTranslation('placeholder')"
+      />
+    </label>
+  </div>
+</template>
+<script setup lang="ts">
+import { SfInput } from '@storefront-ui/vue';
+
+const { updateSetting, getSetting } = useSiteSettings('googleGtmTrackingId');
+
+const googleGtmTrackingId = computed({
+  get: () => getSetting(),
+  set: (value) => updateSetting(value),
+});
+</script>
+
+<i18n lang="json">
+{
+  "en": {
+    "label": "Google Tag Manager ID",
+    "placeholder": "GTM-XXXXXXX"
+  },
+  "de": {
+    "label": "Google Tag Manager ID",
+    "placeholder": "GTM-XXXXXXX"
+  }
+}
+</i18n>
