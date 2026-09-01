0a326caafbbd1340994c87eb6f4ed1766e5e16a4
seoambulance <125657912+seoambulance@users.noreply.github.com>
2026-09-01 13:18:21 +0200

    Create View.vue

---
+++ apps/web/modules/gtm-tracking/runtime/components/settings/gtm-tracking/google-analytics/View.vue
@@ -0,0 +1,7 @@
+<template>
+  <SiteConfigurationView>
+    <template #setting-title>{{ getEditorUITranslation('google-analytics') }}</template>
+  </SiteConfigurationView>
+</template>
+
+<script setup lang="ts"></script>
