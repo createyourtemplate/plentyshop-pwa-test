efa988483accd5a4d9ff9badd433610676f99cbe
seoambulance <125657912+seoambulance@users.noreply.github.com>
2026-09-01 13:16:29 +0200

    Delete apps/web/modules/gtm-tracking/runtime/components/settings/gtm-tracking/google-tag-manager/View.vue

--- apps/web/modules/gtm-tracking/runtime/components/settings/gtm-tracking/google-tag-manager/View.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <SiteConfigurationView>
-    <template #setting-title>{{ getEditorUITranslation('gtm-tracking') }}</template>
-  </SiteConfigurationView>
-</template>
-<script setup lang="ts"></script>
