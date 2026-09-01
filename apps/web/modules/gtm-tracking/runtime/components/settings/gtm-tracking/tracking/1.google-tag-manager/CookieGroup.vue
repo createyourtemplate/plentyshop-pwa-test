24c91ae6a0a763d33a21111365335294287f1541
seoambulance <125657912+seoambulance@users.noreply.github.com>
2026-09-01 15:23:20 +0200

    Update CookieGroup.vue

--- apps/web/modules/gtm-tracking/runtime/components/settings/gtm-tracking/tracking/1.google-tag-manager/CookieGroup.vue
+++ apps/web/modules/gtm-tracking/runtime/components/settings/gtm-tracking/tracking/1.google-tag-manager/CookieGroup.vue
@@ -39,3 +39,18 @@ const googleGtmCookieGroup = computed({
   },
 });
 </script>
+
+<i18n lang="json">
+{
+  "en": {
+    "label": "GTM Tracking",
+    "description1": "⚠️ This group of settings will require a shop redeploy to take effect.",
+    "description2": "Control if and how you want to use Google Tag Manager."
+  },
+  "de": {
+    "label": "GTM Tracking",
+    "description1": "⚠️ Damit diese Einstellungen der Gruppe wirksam wird, ist eine erneute Bereitstellung des Shops erforderlich.",
+    "description2": "Steuern Sie, ob und wie Sie Google Tag Manager nutzen möchten."
+  }
+}
+</i18n>
